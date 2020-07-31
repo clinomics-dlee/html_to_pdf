2f69c3a9959fc4b654d3f0db4974449527d87f4a
clinomics-dlee <dlee@clinomics.co.kr>
2020-07-31 15:26:25 +0900

    argv 값 셋팅

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const puppeteer = require('puppeteer');
 
+console.log(">>>>>> begin - create PDF <<<<<<<");
+console.log("args=" + process.argv[0]);
+console.log("args=" + process.argv[1]);
+
+// #. argv[0] : node, argv[1] : index.js
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
 (async (path, filename) => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.goto('path');
+	await page.goto(url + "?filePath=" + filePath);
 	await page.waitFor(500);
 	await page.pdf({
-		path: 'aa.pdf',
+		path: filePath + '/Result.pdf',
 		preferCSSPageSize: true,
 		printBackground: true,
 		format: 'A4'});
 	await browser.close();
 })();
+
+console.log(">>>>>> end - create PDF <<<<<<<");
